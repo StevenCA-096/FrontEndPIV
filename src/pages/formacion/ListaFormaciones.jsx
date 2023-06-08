0abe2515e561e1475a2991825da94dbc220e403c
JOSE FONSECA MENDEZ <jose.fonseca.mendez@est.una.ac.cr>
2023-06-07 22:15:36 -0600

    Formacion pages

---
+++ src/pages/formacion/ListaFormaciones.jsx
@@ -0,0 +1,81 @@
+
+import { useQuery, useMutation, QueryClient } from 'react-query'
+
+import { getFormacion } from '../../Services/FormacionService'
+import { deleteFormacion } from '../../Services/FormacionService'
+import AgregarFormacion from './components/AgregarFormacion'
+
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router';
+
+const ListaFormaciones = () => {
+
+    const candidatoParams = useParams();
+
+    const [Candidato,setcandidato]= useState(null)
+    const [formacionesCandidato, setFormacionesCandidato] = useState([]);
+  
+    const queryClient = new QueryClient();
+    const { data, isLoading, isError } = useQuery('formacion', getFormacion);
+
+    useEffect(() => {
+        if (data) {
+          const formacionesFiltradas = data.filter(
+            (formacion) => formacion.CandidatoId === candidatoParams.id
+          );
+          console.log('getFormacion:', data);
+          console.log('candidatoParams.id:', candidatoParams.id);
+          console.log('formacionesFiltradas:', formacionesFiltradas);
+          setFormacionesCandidato(formacionesFiltradas);
+        }
+      }, [data, candidatoParams.id]);
+
+    if (isLoading)
+        return <div>Loading...</div>
+
+    if (isError)
+        return <div>Error</div>
+
+        if (formacionesCandidato.length === 0) {
+            return <div>El candidato no tiene formaciones</div>;
+          }
+
+    return (
+        <>
+            <div>
+                <AgregarFormacion /><br />
+                
+            </div>
+            <div>Lista de formaciones</div><br />
+            <div>
+                <table>
+                    <thead >
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Años de estudio</th>
+                            <th>Culminacion</th>
+                            <th>Acciones</th>
+                        </tr>
+
+                        {
+                            formacionesCandidato.map((formacion) =>
+                                <>
+                                    <tr key={formacion.id}>{formacion.Nombre}
+                                        <td>{formacion.Años_Estudio}</td>
+                                        <td>{formacion.Fecha_Culminacion}</td>
+                                        <td>
+                                            <button className='tablebtn' onClick={() => deleteCandidato(candidato.id)}>Eliminar</button>
+                                        </td>
+                                    </tr>
+
+                                </>
+                            )}
+                    </thead>
+                </table>
+
+            </div>
+        </>
+    )
+}
+
+export default ListaFormaciones
---
+++ src/pages/formacion/components/AgregarFormacion.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useRef } from "react";
+import { QueryClient, useMutation } from "react-query";
+import { create } from '../../../Services/FormacionService';
+
+const AgregarFormacion = () => {
+    const queryClient = new QueryClient();
+  
+    const nombre = useRef();
+    const Años_Estudio = useRef();
+    const Fecha_Culminacion = useRef();
+  
+    const mutation = useMutation("formacion",create,
+      {
+          onSettled:()=>queryClient.invalidateQueries("formaciones"),
+          mutationKey: "formacion"
+      })
+  
+      const save = ()=>{
+        let nuevaFormacion = {       
+            nombre:nombre.current.value,
+            Años_Estudio: parseInt(Años_Estudio.current.value),
+            Fecha_Culminacion:Fecha_Culminacion.current.value
+        };
+        mutation.mutateAsync(nuevaFormacion);
+        limpiarInput()
+    }
+    const limpiarInput = ()=>{
+      nombre.current.value = "";
+      Años_Estudio.current.value = "";
+      Fecha_Culminacion.current.value = "";
+    }
+  
+    return (
+      <>
+      <h2>Agregar formacion</h2>
+      <div id='input-container'>
+        
+        <input ref={nombre} type="text" id="nombre" placeholder='Nombre'/>
+       
+        <input placeholder='Años de estudio' ref={Años_Estudio} type="int" id="anStu"/>
+  
+        <input placeholder='Fecha de culminacion' className='input' ref={Fecha_Culminacion} type="text" id="feCu"/>
+
+        </div>
+        <button onClick={save}>Agregar</button>
+           
+      </>
+    )
+  }
+  
+  export default AgregarFormacion
